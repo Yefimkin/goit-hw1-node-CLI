0efb2300f8d2b2564ab5c5a98297ecbe1ee1cc9c
Yefimkin Denys <iefimkindenis@gmail.com>
2022-11-22 17:07:55 +0200

    make some

---
+++ contacts.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+const contactsPath = async () => {};
+
+const filepath = "./db/contacts.json";
+contactsPath({ filepath });
+
+// const contactsPath = require("./db/contacts.json");
+// console.log(contactsPath);
