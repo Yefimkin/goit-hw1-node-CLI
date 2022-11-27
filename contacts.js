4ea70dfa2ec7a7917126dba342d51983128e6ac1
Yefimkin Denys <iefimkindenis@gmail.com>
2022-11-27 17:49:55 +0200

    add code

--- contacts.js
+++ contacts.js
@@ -1,9 +1,58 @@
-const fs = require("fs");
+const fs = require("fs/promises");
+const path = require("path");
 
-const contactsPath = async () => {};
+const contactsPath = path.resolve("db/contacts.json");
 
-const filepath = "./db/contacts.json";
-contactsPath({ filepath });
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactPath, "utf8");
+    const parsedData = JSON.parse(data);
+    return parsedData;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// const contactsPath = require("./db/contacts.json");
-// console.log(contactsPath);
+async function getContactById(contactId) {
+  try {
+    const data = await listContacts();
+    const contact = data.find(({ id }) => id === contactId.toString);
+    console.log(
+      `Contact with this id ${contactId} is: ${JSON.stringify(contact)}`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = await listContacts();
+    const newData = data.filter(({ id }) => id !== contactId.toString());
+    console.log(`Contact this with ${contactId} successfully deleted`);
+    await fs.writeFile(contactsPath, JSON.stringify(newData));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const data = await listContacts();
+    data.push({ name, email, phone });
+    await fs.writeFile(contactsPath, JSON.stringify(data));
+
+    console.log(`Contact ${name} successfully added`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const contactsOperations = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
+
+module.exports = contactsOperations;
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Welcome to Node.js, friends");
+const contactsOperations = require("./contacts");
+
+const argv = require("yargs").argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const data = await listContacts();
+      console.table(data);
+      break;
+
+    case "get":
+      await getContactById(id);
+      break;
+
+    case "add":
+      await addContact(name, email, phone);
+      break;
+
+    case "remove":
+      await removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
